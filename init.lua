09721de38d0f3d6d0e35668d21d851333ce2c7b4
Deftly <deftkz@gmail.com>
2023-03-07 12:10:36 -0800

    autocommands/groups, mappings, options, and settings

--- init.lua
+++ init.lua
@@ -4,3 +4,9 @@
 require("core.globals")
 -- Plugin management via lazy
 require("core.lazy")
+-- "Global" Keymappings
+require("core.mappings")
+-- All non plugin related (vim) options
+require("core.options")
+-- Vim autocommands/autogroups
+require("core.autocmd")
